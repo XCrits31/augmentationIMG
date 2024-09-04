c198e654354e26ab85f19dfc8a4ae0695724405a
Trubaiev Oleksandr <xcrits31@gmail.com>
2024-09-04 06:11:57 +0000

    dd react

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $http = Http::get("https://api.telegram.org/bot" . env('TG_API') . "/setWebhook?url=https://xcrits31.su/webhook");
+    $http = Http::get("https://api.telegram.org/bot" . env('TG_API') . "/getWebhookInfo");
     dd(json_decode($http));
     //return view('welcome');
 });
