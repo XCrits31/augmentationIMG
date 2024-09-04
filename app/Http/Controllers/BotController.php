5bdc8a60658e3a1ab66348c870e25849b771423f
Trubaiev Oleksandr <xcrits31@gmail.com>
2024-09-04 03:40:59 +0000

    Botcontroller, webhook, tguser, sendMessage

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TgUser;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class BotController extends Controller
+{
+
+    public function handleWebhook(Request $request)
+    {
+        $update = $request->all();
+
+        $telegramId = $update['message']['from']['id'];
+        $name = $update['message']['from']['first_name'];
+        $username = $update['message']['from']['username'] ?? null;
+
+        TgUser::updateOrCreate(
+            ['telegram_id' => $telegramId],
+            ['name' => $name, 'username' => $username]
+        );
+
+        $responseMessage = "Hello, {$name}";
+
+        $this->sendMessage($telegramId, $responseMessage);
+    }
+
+    protected function sendMessage($chatId, $message)
+    {
+        $url = "https://api.telegram.org/bot" . env('APP_NAME') . "/sendMessage";
+        $data = [
+            'chat_id' => $chatId,
+            'text' => $message,
+        ];
+
+        Http::post($url, $data);
+    }
+}
