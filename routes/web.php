c11b62e2167fb18cb1d9589cdf195aac39a7ac57
Trubaiev Oleksandr <xcrits31@gmail.com>
2024-09-04 06:09:03 +0000

    dd react

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\BotController;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $http = Http::get("https://api.telegram.org/bot" . env('TG_API') . "/setWebhook?url=https://xcrits31.su/webhook");
+    dd(json_decode($http));
+    //return view('welcome');
 });
 
 Route::post('/webhook', [BotController::class, 'handleWebhook']);
