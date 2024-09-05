5dd81b06d62da1676d08ec91bea4c539b4748875
Trubaiev Oleksandr <xcrits31@gmail.com>
2024-09-05 08:40:33 +0000

    trello test

--- app/Http/Controllers/TrelloWebhookController.php
+++ app/Http/Controllers/TrelloWebhookController.php
@@ -12,8 +12,12 @@ class TrelloWebhookController extends Controller
     public function handle(Request $request)
     {
         Log::info('Trello Webhook Received:', $request->all());
-
-
+        $url = "https://api.telegram.org/bot" . env('TG_API') . "/sendMessage";
+        $data = [
+            'chat_id' => '1050201265',
+            'text' => 'trello',
+        ];
+        Http::post($url, $data);
         return response('Webhook handled', 200);
     }
 }
