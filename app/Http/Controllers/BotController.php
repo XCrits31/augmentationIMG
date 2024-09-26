d1455c1d6ff859bf8592daf73a4b3ed9426d26b9
Trubaiev Oleksandr <xcrits31@gmail.com>
2024-09-26 15:53:43 +0000

    api

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -6,6 +6,7 @@ use App\Models\TgUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Client;
 
 class BotController extends Controller
 {
