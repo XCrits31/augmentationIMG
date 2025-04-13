e0df3374349923e43823bf7e1cd0371058724332
Oleksandr Trubaiev <xcrits31@gmail.com>
2025-04-13 15:54:04 +0200

    process

--- scripts/monai_processing.py
+++ scripts/monai_processing.py
@@ -34,7 +34,7 @@ def main():
     # Пайплайн преобразования: загрузка, добавление канала, масштабирование интенсивности, изменение размера
     transforms = Compose([
         LoadImage(image_only=True),
-        #Rotate(angle=90, mode='nearest', keep_size=True),
+        Rotate(angle=90, mode='nearest', keep_size=False),
         ToTensor(),
     ])
 
