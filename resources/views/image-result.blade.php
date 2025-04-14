40a9f60c5f93f5cbace12e6fe8079de2258dfcb6
Oleksandr Trubaiev <xcrits31@gmail.com>
2025-04-14 22:28:25 +0200

    scrips

--- resources/views/image-result.blade.php
+++ resources/views/image-result.blade.php
@@ -10,8 +10,8 @@
 <h2>Исходное изображение:{{ $originalUrl }}"</h2>
 <img src="{{ $originalUrl }}" alt="Исходное изображение" style="max-width: 45%;">
 
-<h2>Обработанное изображение ({{ $transformation }}):{{$processedUrl}}</h2>
-<img src="{{ $processedUrl }}" alt="Обработанное изображение" style="max-width: 45%;">
+<h2>Обработанное изображение ({{ $transformation }}):{{outputPath}}</h2>
+<img src="{{ outputPath }}" alt="Обработанное изображение" style="max-width: 45%;">
 
 <br><br>
 <a href="/upload-image">Загрузить другое изображение</a>
