10a15d54fd7c394964568b60df63604081ce8570
Oleksandr Trubaiev <xcrits31@gmail.com>
2025-04-15 09:42:02 +0200

    scrips

--- resources/views/image-result.blade.php
+++ resources/views/image-result.blade.php
@@ -9,7 +9,35 @@
 
 <h2>Исходное изображение:{{ $originalUrl }}"</h2>
 <img src="{{ $originalUrl }}" alt="Исходное изображение" style="max-width: 45%;">
-({{ $transformations }})
+<h2>Применённые трансформации:</h2>
+@if(!empty($transformations))
+    <ul>
+        @foreach($transformations as $transformation)
+            <li>
+                <strong>{{ ucfirst($transformation['transformation']) }}</strong>
+                @if(!empty($transformation['parameters']))
+                    <ul>
+                        @foreach($transformation['parameters'] as $paramName => $paramValue)
+                            <li>
+                                {{ ucfirst($paramName) }}:
+                                @if(is_bool($paramValue))
+                                    {{ $paramValue ? 'true' : 'false' }}
+                                @else
+                                    {{ $paramValue }}
+                                @endif
+                            </li>
+                        @endforeach
+                    </ul>
+                @else
+                    (No parameters)
+                @endif
+            </li>
+        @endforeach
+    </ul>
+@else
+    <p>Трансформации не были выбраны.</p>
+@endif
+
 <h2>Обработанное изображение :{{$outputPath}}</h2>
 <img src="{{ $outputPath }}" alt="Обработанное изображение" style="max-width: 45%;">
 
