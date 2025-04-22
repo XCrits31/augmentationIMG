c28fff13a770306e248701d0929b4719cda600d5
Oleksandr Trubaiev <xcrits31@gmail.com>
2025-04-22 19:25:58 +0200

    broadcastinng

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\ProcessImageCompleted;
 use App\Http\Controllers\BotController;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -12,4 +13,8 @@ Route::post('/process-image', [ImageProcessingController::class, 'processImage']
 Route::get('/transformations', [ImageProcessingController::class, 'showTransformations'])->name('transformations.index');
 Route::delete('/transformations/{id}', [ImageProcessingController::class, 'deleteTransformation'])->name('transformations.delete');
 Route::delete('/transformations', [ImageProcessingController::class, 'deleteAllTransformations'])->name('transformations.deleteAll');
+Route::get('/test-event', function () {
+    event(new ProcessImageCompleted(['message' => 'Test event message!']));
+    return 'Event has been sent!';
+});
 
