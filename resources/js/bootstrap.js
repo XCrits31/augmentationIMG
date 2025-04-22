6a2e3c7d2a8fa5d9cc95f19da674ad6278e5a76d
Oleksandr Trubaiev <xcrits31@gmail.com>
2025-04-22 15:10:25 +0200

    broadcastinng

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,16 +4,16 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
-console.log("KEY:", import.meta.env.VITE_PUSHER_APP_KEY);
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: '2b5d64a15fe154fa385d',
-    cluster: 'eu',
-    wsHost: window.location.hostname,
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    wsHost: 'xcrits31.su',
     wsPort: 6001,
     wssPort: 6001,
     forceTLS: true,
+    encrypted: true,
     disableStats: true,
 });
 
