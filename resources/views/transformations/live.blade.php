c1d0447837ab5ae478ee3d2f027200c798d241f9
Oleksandr Trubaiev <xcrits31@gmail.com>
2025-04-23 06:06:44 +0200

    broadcastinng

--- resources/views/transformations/live.blade.php
+++ resources/views/transformations/live.blade.php
@@ -14,6 +14,19 @@
         .image-card img {
             max-width: 100%;
         }
+        .card {
+            width: 100%;
+            max-width: 300px;
+            word-wrap: break-word;
+            overflow: hidden;
+        }
+
+        .card-body {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: normal; /* или 'nowrap' если хочешь в одну строку */
+            word-break: break-word;
+        }
     </style>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
